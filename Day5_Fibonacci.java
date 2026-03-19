5b2e60b7ecbd82a6f57b6ae44dc2a99bce16e645
Swati Mallayya bhikshavatimath <swatib0306@gmail.com>
2026-03-19 18:57:09 +0530

    Implement Fibonacci series in Day5_Fibonacci.java

---
+++ Day5_Fibonacci.java
@@ -0,0 +1,13 @@
+public class Day_Fibonacci{
+  public static void main(String[]args){
+    int n=10;
+    int a=0,b=1;
+    System.out.println("Fibonacci series:");
+    for(int i=1;i<=n;i++){
+      System.out.println(a+" ");
+      int next=a+b;
+      b=next;
+    }
+  }
+}
+      
