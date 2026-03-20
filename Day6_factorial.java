aeecc393a0dd93d63b4b3e4ece1e139b11e31c05
Swati Mallayya bhikshavatimath <swatib0306@gmail.com>
2026-03-20 23:58:00 +0530

    Implement factorial calculation in Java
    Fix typo in print statement and correct loop increment.

---
+++ Day6_factorial.java
@@ -0,0 +1,10 @@
+public class Day6_Factorial {
+public static void main(String[] args){
+int num =5;
+int fact=1;
+for(int i=1;i<=num;i++){
+fact=fact*i;
+}
+System.out.prinltn("Factorial of"+num+"is:"+fact);
+}
+}
