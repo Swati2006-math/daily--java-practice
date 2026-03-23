7dc180c03a29e829fd5481cf679be31b4a0dcd0c
Swati Mallayya bhikshavatimath <swatib0306@gmail.com>
2026-03-23 13:18:01 +0530

    Add Java program to calculate sum of digits

---
+++ Day9_sumofDigits.java
@@ -0,0 +1,13 @@
+public class Day9_sumofDigits{
+public static void main(String[]args)
+{
+int num=1234;
+int sum=0;
+while(num!=0){
+int digit=num%10;
+sum=sum+digit;
+num=num/10;
+}
+System.out.println("sum of digits:"+sum);
+}
+}
