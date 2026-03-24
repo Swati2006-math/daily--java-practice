f0892f4c3ab5989aa364da6ac19b03b768385d71
Swati Mallayya bhikshavatimath <swatib0306@gmail.com>
2026-03-25 00:09:48 +0530

    Implement Armstrong number check in Day10_Armstrong
    Fix logic errors and print statement in Armstrong number check.

---
+++ Dat10_Armstrong.java
@@ -0,0 +1,16 @@
+public class Day10_Armstrong{
+  public static void main(String[] args){
+    int num=153;
+    int original=num;
+    int sum=0;
+    while(num!0){
+      int digit=num%10
+        sum=sum+(digit*digit*digit);
+      num=num/10;
+    }
+    if(sum==original)
+    System.out.println("Armstrong number");
+    else
+  Sysrem.out.println("Not an Armstrong number");
+  }
+}
