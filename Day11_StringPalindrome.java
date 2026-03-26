0d0f8d7eb37fc756120ad6377ea283b81f3a0f81
Swati Mallayya bhikshavatimath <swatib0306@gmail.com>
2026-03-26 15:19:17 +0530

    Implement palindrome check in Day11_StringPalindrome
    Fixes variable naming and logic errors in palindrome check.

---
+++ Day11_StringPalindrome.java
@@ -0,0 +1,13 @@
+public class Day11_StringPalindrome{
+  public static void main(String[]args){
+    string str="madam";
+    String reversed="";
+    for(int i=str.length()-1;i>=0;i__){
+      reversed=reversed+str.charAt(i);
+    }
+    if(str.equals(revesed))
+      System.out.println("Palindrome string");
+    else
+      Sustem.out.println("Not a palindrome");
+  }
+}
