56b1ac47eba69b34e0486cc954376a605f1898a2
Swati Mallayya bhikshavatimath <swatib0306@gmail.com>
2026-04-15 23:12:40 +0530

    Add LongestWord class to find longest word in string

---
+++ Day17_LonestWord.java
@@ -0,0 +1,16 @@
+public class LongestWord {
+    public static void main(String[] args) {
+        String str = "I love programming in Java";
+
+        String[] words = str.split(" ");
+        String longest = "";
+
+        for (String word : words) {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+
+        System.out.println("Longest word: " + longest);
+    }
+}
